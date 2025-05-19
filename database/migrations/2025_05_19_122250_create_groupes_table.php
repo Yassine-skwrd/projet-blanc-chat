ffc91bc9526f0cbc5b63316e274a0dfc711230ba
Yassine-skwrd <bourhim.yass@gmail.com>
2025-05-19 14:44:58 +0200

    Ajout 'nom', 'description', and 'group_admin_id' fields to 'groupes' table migration

--- database/migrations/2025_05_19_122250_create_groupes_table.php
+++ database/migrations/2025_05_19_122250_create_groupes_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('groupes', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('nom');
+            $table->string('description')->nullable();
+            $table->foreignId('group_admin_id')->constrained('users')->onDelete('cascade');
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
